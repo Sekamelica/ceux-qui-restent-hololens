69f24303740ff06e1b99b0da28f5a5e0e16b62b5
Sekamelica <alice-pnt@hotmail.com>
2018-06-02 01:53:18 +0200

    Adjustements on silhouettes

---
+++ Assets/Ceux Qui Restent/Scripts/Tools/Editor/LinkablePositionnerWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace CeuxQuiRestent.Tools
+{
+    public class LinkablePositionnerWindow : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        [MenuItem("CONTEXT/Rigidbody/New Option")]
+        private static void NewOpenForRigidBody()
+        {
+        }
+    }
+}
+#endif
